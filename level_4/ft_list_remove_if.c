270c2f95a72f14b823730e631aa371f02c93d005
Fredric Renna <frenna@oa-j6.21-school.ru>
2019-11-12 09:24:59 +0300

    Fix ft_list_remove

--- level_4/ft_list_remove_if.c
+++ level_4/ft_list_remove_if.c
@@ -9,21 +9,25 @@ void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)());
 cmp takes two void* and returns 0 when both parameters are equal.
 */
 
-typedef struct      s_list
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct	s_list
 {
-    struct s_list   *next;
-    void            *data;
-}                   t_list;
+	struct s_list	*next;
+  void 					*data;
+}								t_list;
 
 void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 {
-	t_list curr;
-	t_list tmp;
+	t_list *curr;
+	t_list *tmp;
 
 	if (*begin_list && cmp((*begin_list)->data, data_ref) == 0)
 	{
 		curr = *begin_list;
-		*begin_list = *begin_list->next;
+		*begin_list = (*begin_list)->next;
 		free(curr);
 	}
 	curr = *begin_list;
@@ -38,4 +42,77 @@ void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 		else
 			curr = curr->next;
 	}
+}
+
+void	new_list(t_list **begin, int d)
+{
+	t_list	*new;
+	t_list	*curr;
+
+	new = (t_list *)malloc(sizeof(*new));
+	if (!new)
+		return ;
+	new->data = d;
+	new->next = NULL;
+	if (*begin == NULL)
+	{
+		*begin = new;
+		return ;
+	}
+	curr = *begin;
+	while (curr->next != NULL)
+		curr = curr->next;
+	curr->next = new;
+	return ;
+}
+
+int		compare(int a, int b)
+{
+	if (a < b)
+		return (0);
+	else
+		return (1);
+}
+
+void	print(t_list *start)
+{
+	t_list	*curr;
+	int i;
+
+	i = 6;
+	curr = start;
+	while (i >= 0 && curr->next != NULL)
+	{
+		printf("%d ", curr->data);
+		curr = curr->next;
+		i--;
+	}
+	printf("%d\n", curr->data);
+}
+
+void	swap(t_list *a, t_list *b)
+{
+	int	tmp;
+
+	tmp = a->data;
+	a->data = b->data;
+	b->data = tmp;
+}
+
+int				main()
+{
+	int 		a[7] = {4, 7, 1, 3, 2, 5, 6};
+	t_list	*start;
+	int i;
+
+	i = 0;
+	start = NULL;
+	while (i < 7)
+	{
+		new_list(&start, a[i]);
+		i++;
+	}
+	print(start);
+	
+	return (0);
 }
